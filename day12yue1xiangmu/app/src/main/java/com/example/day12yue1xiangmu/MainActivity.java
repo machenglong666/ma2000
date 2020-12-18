a1f198d6b2b657289fb39beb87f6c0a5e9b04337
马成龙” <1789113287@qq.com>
2020-12-18 10:58:39 +0800

    我还是宋立宏的爹

---
+++ day12yue1xiangmu/app/src/main/java/com/example/day12yue1xiangmu/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.day12yue1xiangmu;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText name;
+    private EditText pass;
+    private EditText password;
+    private Button denglv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+
+    }
+
+
+
+
+}
